7e37b2bdb8e348543ab7ab51d5b2f7fae7adba19
Vivekumar01 <vivekumar2003bsr@gmail.com>
2023-03-06 11:29:09 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+dotenv.config({ path: './config.env' })
+require('./db/conn')
+app.use(express.json());
+app.use(bodyParser.urlencoded(
+  { extended: true }
+))
+
+app.use(cors());
+app.use(express.json());
+// app.set("view engine", "ejs")
+
+
+app.get("/",async(req,res)=>{
+  res.json("Congratulations Plot Listing App Deployed successfully")
+})
+
+// app.use(require('./router/api'));
+
+const port = process.env.PORT || 50000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
